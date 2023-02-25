eed9e5227fc0d0c16f497bf924e538ab67ab81cb
AfGAY <chernienko.13@mail.ru>
2023-02-25 16:01:15 +0700

    Сделали задание 1.1

--- lab_1_1.py
+++ lab_1_1.py
@@ -1,3 +1,4 @@
+#пункт 1
 a = int(input('Первое число'))
 b = int(input('Второе число'))
 c = int(input('Третье число'))
@@ -7,3 +8,28 @@ elif a >= b <= c:
     print(b)
 elif a >= c <=b:
     print(c)
+#пункт 2
+a = int(input('Первое число'))
+b = int(input('Второе число'))
+c = int(input('Третье число'))
+if 1<= a <=50:
+    print(a);
+if 1 <= b <= 50:
+    print(b);
+if 1 <= c <=50:
+    print(c);
+#пункт 3
+m = float(input('Введите вещественное число m: '))
+
+for x in range(1, 11):
+    print(x*m)
+#пункт 4
+g = 0
+q = 0
+num = float(input('Введите число: '))#чтобы закончить последовательность введите 0
+while num != 0:
+    g += num
+    q += 1
+    num = float(input('Введите число: '))
+print('Сумма: ', g)
+print('Количество: ', q)
