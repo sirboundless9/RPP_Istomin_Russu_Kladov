48c23978fbc309d5086ea4fc6961ac0d51f92e60
AfGAY <chernienko.13@mail.ru>
2023-02-25 14:57:25 +0700

    Сделали задание 1.1 пункт 1

---
+++ lab_1_1.py
@@ -0,0 +1,9 @@
+a = int(input('Первое число'))
+b = int(input('Второе число'))
+c = int(input('Третье число'))
+if b>= a <=c:
+    print(a)
+elif a >= b <= c:
+    print(b)
+elif a >= c <=b:
+    print(c)
