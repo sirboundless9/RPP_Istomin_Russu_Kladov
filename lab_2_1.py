c23625b207f9e2bf963d726305a7414901d3b398
sirboundless9 <115053571+sirboundless9@users.noreply.github.com>
2023-03-25 14:42:03 +0700

    Update lab_2_1.py

--- lab_2_1.py
+++ lab_2_1.py
@@ -1,4 +1,3 @@
 stroka = str(input('Введите строку'))
 stroka_future = stroka.split()
-print(sum(map(lambda x:
-              x.startswith('m') + x.startswith('M'), stroka_future)))
+print(sum(map(lambda x: x.startswith('m') + x.startswith('M'), stroka_future)))
